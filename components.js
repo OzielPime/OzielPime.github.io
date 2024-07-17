a5d1a98df49a2dfa41156bd7a82615ee9688434d
OzielPime <139709735+OzielPime@users.noreply.github.com>
2024-07-16 23:13:44 -0300

    Add files via upload

--- components.js
+++ components.js
@@ -1,3 +1,7 @@
+const numeroWhatsApp= "5542999738953";//"5542991467224"
+var nomeSalvo = localStorage.getItem('nomeVisitante');
+var mensagem='';
+
 // Função para carregar o cabeçalho
 function loadHeader() {
     const headerContainer = document.getElementById('header-container');
@@ -6,10 +10,31 @@ function loadHeader() {
     xhr.open('GET', 'header.html', true);
     xhr.onload = function() {
         if (xhr.status === 200) {
+			
             headerContainer.innerHTML = xhr.responseText;
+			
+				const whatsappLinkHeader = document.getElementById('botaoWhatsHeader');
+				// Se o nome não estiver salvo
+				if (!nomeSalvo) {
+					//mostrarPopup();
+					mensagem = `Oi, eu vim do seu site`;
+				} else {
+					// Se o nome estiver salvo
+					//mostrarNomeSalvo();
+					mensagem = `Oi! Me chamo ${nomeSalvo}, eu vim do seu site`;
+				}				
+
+				// Define o valor do atributo href da âncora"https://wa.me/message/3JE4GCAF6ERHP1";//
+				const urlHeader = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensagem)}`;
+				whatsappLinkHeader.href = urlHeader;
         }
     };
+
+
+			
     xhr.send();
+	
+
 }
 
 // Função para carregar o rodapé
@@ -25,3 +50,20 @@ function loadFooter() {
     };
     xhr.send();	
 }
+
+
+function DadosDoCliente() {
+	// Se o nome não estiver salvo, mostra o pop-up
+	if (!nomeSalvo) {
+		mostrarPopup();
+	} else {
+		// Se o nome estiver salvo, mostra na mensagem
+		mostrarNomeSalvo();
+	}
+}
+		// Verifica se o nome do visitante já foi salvo
+        window.onload = function() {
+            
+
+            			
+        };
